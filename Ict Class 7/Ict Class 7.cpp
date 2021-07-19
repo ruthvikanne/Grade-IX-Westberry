c7fce4b9252be59306b032820c5c16594332c316
Ruthvik Anne <admin@Trobo956.onmicrosoft.com>
2021-07-19 12:49:40 +0530

    Add Project Files

---
+++ Ict Class 7/Ict Class 7.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include<conio.h>
+main()
+{
+    int maths, sci, soc, tot, avg;
+    char res[20];
+    printf("Enter Marks in Maths   ");
+    scanf("%d", &maths);
+    printf("Enter Marks in Science   ");
+    scanf("%d", &sci);
+    printf("Enter Marks in Social   ");
+    scanf("%d", &soc);
+
+    tot = maths + sci + soc;
+    avg = tot / 3;
+
+    if (avg >= 90)
+    {
+        printf("You got First Class");
+    }
+    else if (avg >= 80)
+    {
+        printf("You got Second Class");
+    }
+    else if (avg >= 60)
+    {
+        printf("You got Third Class");
+    }
+    else
+    {
+        printf("Fail");
+    }
+
+}
