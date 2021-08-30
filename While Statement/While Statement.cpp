cefea72726d85327524357bb1d8667ce4985589b
Ruthvik Anne <admin@Trobo956.onmicrosoft.com>
2021-08-30 13:11:47 +0530

    Code Edited

--- While Statement/While Statement.cpp
+++ While Statement/While Statement.cpp
@@ -1,15 +1,17 @@
 // While Statement.cpp : This file contains the 'main' function. Program execution begins and ends there.
 // How To Use While Statement
 
-#include <iostream>
+#include <stdio.h>
 
 int main()
 {
-    int i = 1;
+    int i = 1,sum = 0;
     while (i <= 1)
     {
-        printf("%d", i);
-        i = i + 1;
-    }
-}
+        printf("%d \t", i);
+        i ++;
 
+        sum = sum + i;
+    }
+
+}
