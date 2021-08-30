ad1b2b8e4ad5bf8be2619ab66aace28d9e67350c
Ruthvik Anne <admin@Trobo956.onmicrosoft.com>
2021-08-30 12:57:47 +0530

    New Project Added

---
+++ While Statement/While Statement.cpp
@@ -0,0 +1,15 @@
+// While Statement.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// How To Use While Statement
+
+#include <iostream>
+
+int main()
+{
+    int i = 1;
+    while (i <= 1)
+    {
+        printf("%d", i);
+        i = i + 1;
+    }
+}
+
